337571ed48c10b5102ac6a677cba0d80397ad830
nikolay <fcttmk@yandex.ru>
2020-12-28 17:05:14 +0700

    zadanue1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-let number = 10;
+const numberOfFilms = +prompt("Сколько фильмов Вы посмотрели?", "");
 
-number = 15;
+const personalMovieDB = {
 
-console.log(number);
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ""),
+      b = prompt('Какую оценку Вы поставили фильму?',""),
+      c = prompt('Один из последних просмотренных фильмов?', ""),
+      d = prompt('Какую оценку Вы поставили фильму?',"");
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
