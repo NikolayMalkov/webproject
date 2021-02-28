2308d8fa30d0bc8e07e8b73cf25dc42273bd4cfd
Nikolay <fcttmk@yandex.ru>
2021-02-28 18:49:35 +0700

    urokfunction

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-const numberOfFilms = +prompt("Сколько фильмов Вы посмотрели?", "");
+let numberOfFilms;
+
+function start() {
+
+    numberOfFilms = +prompt("Сколько фильмов Вы посмотрели?", "");
+        while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+            numberOfFilms = +prompt("Сколько фильмов Вы посмотрели?", "");
+
+        }
+}
+start();
 
 const personalMovieDB = {
 
@@ -11,15 +21,19 @@ const personalMovieDB = {
 
 
 
-for (let i = 0; i < 2; i++)
+
+
+function rememberMyFilms(){
+
+    for (let i = 0; i < 2; i++)
         {
             const a = prompt('Один из последних просмотренных фильмов?', ""),
-                     b = prompt('Какую оценку Вы поставили фильму?',"");
+                  b = prompt('Какую оценку Вы поставили фильму?',"");
             
            personalMovieDB.movies[a] = b;
 
            if (a != null && b != null && a != '' && b != '' && a.length < 50){
-               personalMovieDB.movies[a]=b;
+               personalMovieDB.movies[a] = b;
                console.log('done');
            }
                 else {
@@ -27,5 +41,9 @@ for (let i = 0; i < 2; i++)
                     i--;
                 }
         }
+
+}
+
+rememberMyFilms();
 
 console.log(personalMovieDB);
