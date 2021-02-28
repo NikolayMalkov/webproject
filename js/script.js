0768945916668d60d8797d264dd805c68c76e7f4
Nikolay <fcttmk@yandex.ru>
2021-02-28 11:43:06 +0700

    jscicles

--- js/script.js
+++ js/script.js
@@ -9,13 +9,23 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ""),
-      b = prompt('Какую оценку Вы поставили фильму?',""),
-      c = prompt('Один из последних просмотренных фильмов?', ""),
-      d = prompt('Какую оценку Вы поставили фильму?',"");
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++)
+        {
+            const a = prompt('Один из последних просмотренных фильмов?', ""),
+                     b = prompt('Какую оценку Вы поставили фильму?',"");
+            
+           personalMovieDB.movies[a] = b;
+
+           if (a != null && b != null && a != '' && b != '' && a.length < 50){
+               personalMovieDB.movies[a]=b;
+               console.log('done');
+           }
+                else {
+                    console.log('error');
+                    i--;
+                }
+        }
 
 console.log(personalMovieDB);
